60c2583436c64427e314eeb084383586c4450365
Raden Mas Abdul <mac@MacBook-Air.local>
2026-01-23 14:07:19 +0700

    chore(navbar): adjustment z index

--- src/components/layout/layout/navbar/Navbar.tsx
+++ src/components/layout/layout/navbar/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
   }, []);
 
   return (
-    <header className="sticky top-0 flex w-full bg-surface border-default z-99999 lg:border-b">
+    <header className="sticky top-0 flex w-full bg-surface border-default z-40 lg:border-b">
       <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
         <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-default sm:gap-4 lg:border-b-0 lg:px-0 lg:py-4">
           <div className="flex items-center gap-3">
