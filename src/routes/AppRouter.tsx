012c38dd20a6798070033db8bd8287a5833dcca2
radenmasabdul <abdul.tamsis@gmail.com>
2026-03-17 22:58:11 +0700

    feat(task): implement task page & hooks

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -9,6 +9,7 @@ import TeamsPage from "@/pages/teams/TeamPage"
 import TeamsDetail from "@/features/teams/pages/detail/TeamsDetail"
 import ProjectsPage from "@/pages/projects/ProjectsPage"
 import ProjectDetail from "@/features/projects/pages/detail/ProjectDetail"
+import TaskPage from "@/features/tasks/pages/TaskPage"
 
 import NotFound from "@/pages/NotFound"
 
@@ -27,6 +28,7 @@ export default function AppRouter() {
             <Route path="/teams/:id" element={<TeamsDetail />} />
             <Route path="/projects" element={<ProjectsPage />} />
             <Route path="/projects/:id" element={<ProjectDetail />} />
+            <Route path="/tasks" element={<TaskPage />} />
           </Route>
         </Route>
         
